279b102b415e7379d8de874c5e0b2a663a339146
NikuDev <niek_kuijken@hotmail.com>
2020-01-22 09:25:27 +0100

    Start rendering project-lists - added ProjectCollection.cs for managing the (rendered) lists - added basic add/remove functions to this class
    Signed-off-by: NikuDev <niek_kuijken@hotmail.com>

--- app.ts
+++ app.ts
@@ -1,5 +1,6 @@
 console.log("TS.DragAndDrop started!");
 
+
 /*  Autobind decorator, this is to REPLACE having to call '.bind(this)' on handlers 
    
     this.formEl.bind(this) <- is important so we bind the 'this' that will be used
@@ -57,21 +58,88 @@ function validateObject(validatableInput: IValidatable): boolean{
     return isValid;
 }
 
+class ProjectCollection {
+    projectListTemplate: HTMLTemplateElement;
+    singleProjectTemplate: HTMLTemplateElement;
+
+    divAppHostEl = document.getElementById("app")! as HTMLDivElement;
+    
+    private projects: Project[] = [];
+
+    constructor(){
+        // list template (div containing '<ul>' somewhere)
+        this.projectListTemplate = document.getElementById("project-list")! as HTMLTemplateElement;
+        // single list element template ('<li>')
+        this.singleProjectTemplate = document.getElementById("single-project")! as HTMLTemplateElement;
+
+        // ensure browser compatibility
+        if('content' in document.createElement('template')){
+            var clone = this.projectListTemplate.content.cloneNode(true);
+            
+            this.divAppHostEl.appendChild(clone);
+        }
+    }
+    
+    public addProject(project: Project) {
+        if(!this.hasProject(project)){
+            this.projects.push(project);
+
+            this.appendProjectToList();
+        }
+    }
+
+    appendProjectToList() {
+        var clone = this.singleProjectTemplate.content.cloneNode(true)!;
+        let liEl = clone.firstChild as HTMLUListElement;
+        liEl.textContent = 'hello World!';
+        var ulHtmlEl = this.divAppHostEl.querySelector('ul')!;
+        // ulHtmlEl.textContent = 'Hello World!';
+        ulHtmlEl.appendChild(liEl);
+    }
+    
+    public removeProject(project: Project){
+        if (!this.hasProject(project)){
+            let indexToRemove = this.projects.indexOf(project);
+            
+            if(indexToRemove > -1){
+                this.projects.splice(indexToRemove, 1);
+            }
+        }
+    }
+    
+    public getProjects(){
+        return this.projects;
+    }
+    
+    private hasProject(project: Project): boolean{
+        for(var proj of this.projects){
+            if (proj.Title === project.Title &&
+                proj.Description === project.Description &&
+                proj.People === project.People){
+                    return true;
+                }
+            }
+            
+        return false;
+    }
+}
+
+const projectCollection: ProjectCollection = new ProjectCollection();
 
 class ProjectFormInput {
-  // Encapsulating elements
-  templateFormInputEl: HTMLTemplateElement;
-  formEl: HTMLFormElement;
-  divAppHostEl: HTMLDivElement; // <- using the more generic 'HTMLElement' would also be fine
+    // Encapsulating elements
+    templateFormInputEl: HTMLTemplateElement;
+    formEl: HTMLFormElement;
+    divAppHostEl: HTMLDivElement; // <- using the more generic 'HTMLElement' would also be fine
 
-  titleInputEl: HTMLInputElement;
-  descrTextAreaInputEl: HTMLInputElement;
-  peopleInputEl: HTMLInputElement;
-  
-  /** Initializes the DOM elements needed for the <form> input
-   *  note: this should have some null checking in a real application
-   */
-  constructor() {
+    titleInputEl: HTMLInputElement;
+    descrTextAreaInputEl: HTMLInputElement;
+    peopleInputEl: HTMLInputElement;
+    
+    /** Initializes the DOM elements needed for the <form> input
+     *  note: this should have some null checking in a real application
+     */
+    constructor() {
         this.templateFormInputEl = document.getElementById('project-input')! as HTMLTemplateElement;
         this.divAppHostEl = document.getElementById('app')! as HTMLDivElement;
 
@@ -114,12 +182,13 @@ class ProjectFormInput {
             let createdProject = new Project(title, description, people);
     
             // validateInput
-            if (!validateObject({ value: title, required: true, minLength: 5, maxLength: 10 }) ||
+            if (!validateObject({ value: title, required: true, minLength: 2, maxLength: 10 }) ||
                 !validateObject({ value: description, required: true, maxLength: 128 }) ||
                 !validateObject({ value: people, min: 3, max: 10 })){
                 alert('Invalid Input!');
                 return;
             } else {
+                projectCollection.addProject(createdProject);
                 console.log(`Project created: ${createdProject.Title}`);
                 this.clearInput();
             }
@@ -147,7 +216,7 @@ class Project {
     get Title() {
         return this._title;
     }
-
+    
     set Title(value: string){
         if(value.length <= 0){
             console.warn('Title must be > 0 characters long');
