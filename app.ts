639457318808d0b65a5b38ac58508de9f7d04bfc
NikuDev <niek_kuijken@hotmail.com>
2020-01-22 10:09:54 +0100

    Adjusted rendering of the <ul> elements - added dynamic 'type' of list (active or finished) - rendering the list <section> in a similar way as the form input elements
    Signed-off-by: NikuDev <niek_kuijken@hotmail.com>

--- app.ts
+++ app.ts
@@ -1,6 +1,5 @@
 console.log("TS.DragAndDrop started!");
 
-
 /*  Autobind decorator, this is to REPLACE having to call '.bind(this)' on handlers 
    
     this.formEl.bind(this) <- is important so we bind the 'this' that will be used
@@ -59,25 +58,44 @@ function validateObject(validatableInput: IValidatable): boolean{
 }
 
 class ProjectCollection {
-    projectListTemplate: HTMLTemplateElement;
-    singleProjectTemplate: HTMLTemplateElement;
+    templateProjectList: HTMLTemplateElement;
+    templateSingleProject: HTMLTemplateElement;
+    divAppHostEl: HTMLDivElement;
 
-    divAppHostEl = document.getElementById("app")! as HTMLDivElement;
+    // there is no explicit 'HTMLSectionElement', so we can use HTMLElement
+    projectListSectionEl: HTMLElement;
     
     private projects: Project[] = [];
 
-    constructor(){
+    constructor(private type: 'active' | 'finished') {
         // list template (div containing '<ul>' somewhere)
-        this.projectListTemplate = document.getElementById("project-list")! as HTMLTemplateElement;
+        this.templateProjectList = document.getElementById("project-list")! as HTMLTemplateElement;
         // single list element template ('<li>')
-        this.singleProjectTemplate = document.getElementById("single-project")! as HTMLTemplateElement;
+        this.templateSingleProject = document.getElementById("single-project")! as HTMLTemplateElement;
+        this.divAppHostEl = document.getElementById("app")! as HTMLDivElement
 
-        // ensure browser compatibility
-        if('content' in document.createElement('template')){
-            var clone = this.projectListTemplate.content.cloneNode(true);
-            
-            this.divAppHostEl.appendChild(clone);
-        }
+        const importedNode = document.importNode(this.templateProjectList.content, true);
+        this.projectListSectionEl = importedNode.firstElementChild as HTMLElement;
+
+        // let's apply a style based on whether the projectlist is 'active' or 'finished'
+        this.projectListSectionEl.id = `${type}-project`;
+
+        this.renderProjectList();
+        this.renderContent();
+    }
+
+    private renderProjectList() {
+        this.divAppHostEl.insertAdjacentElement('beforeend', this.projectListSectionEl);
+    }
+
+    private renderContent() {
+        /* to be able to access the <ul> later, we want to use a specific id (based on type in this case) */
+        const listId = `${this.type}-project-list`;
+        /* get the first (and only) <ul> and set the id */
+        this.projectListSectionEl.querySelector('ul')!.id = listId;
+        /* set the header to the used type, i.e. 'ACTIVE PROJECTS' */
+        this.projectListSectionEl.querySelector('h2')!.textContent = 
+            this.type.toString().toUpperCase() + ' PROJECTS';
     }
     
     public addProject(project: Project) {
@@ -89,7 +107,7 @@ class ProjectCollection {
     }
 
     appendProjectToList() {
-        var clone = this.singleProjectTemplate.content.cloneNode(true)!;
+        var clone = this.templateProjectList.content.cloneNode(true)!;
         let liEl = clone.firstChild as HTMLUListElement;
         liEl.textContent = 'hello World!';
         var ulHtmlEl = this.divAppHostEl.querySelector('ul')!;
@@ -124,14 +142,13 @@ class ProjectCollection {
     }
 }
 
-const projectCollection: ProjectCollection = new ProjectCollection();
 
 class ProjectFormInput {
     // Encapsulating elements
     templateFormInputEl: HTMLTemplateElement;
     formEl: HTMLFormElement;
     divAppHostEl: HTMLDivElement; // <- using the more generic 'HTMLElement' would also be fine
-
+    
     titleInputEl: HTMLInputElement;
     descrTextAreaInputEl: HTMLInputElement;
     peopleInputEl: HTMLInputElement;
@@ -142,32 +159,32 @@ class ProjectFormInput {
     constructor() {
         this.templateFormInputEl = document.getElementById('project-input')! as HTMLTemplateElement;
         this.divAppHostEl = document.getElementById('app')! as HTMLDivElement;
-
+        
         const importedNode = document.importNode(this.templateFormInputEl.content, true);
         this.formEl = importedNode.firstElementChild as HTMLFormElement;
         /** 'user-input' is a defined style in the app.css, we apply styling by 
          * setting the id here, but it could also be hard-coded in the html
          */
         this.formEl.id = 'user-input'; 
-
+        
         this.titleInputEl = this.formEl.querySelector("#title") as HTMLInputElement;
         this.descrTextAreaInputEl = this.formEl.querySelector("#description") as HTMLInputElement;
         this.peopleInputEl = this.formEl.querySelector("#people") as HTMLInputElement;
-
+        
         this.registerHandlers();
-
+        
         // We've finished setting everything we need for the form, render it
         this.renderFormElement();
     }
-
+    
     private registerHandlers() {
-           this.formEl.addEventListener('submit', this.handleSubmit);
+        this.formEl.addEventListener('submit', this.handleSubmit);
     }
     
     private renderFormElement() {
         this.divAppHostEl.insertAdjacentElement('afterbegin', this.formEl);
     }
-
+    
     @AutoBind
     private handleSubmit(event: Event) {
         // start with preventing sending of a HTTPRequest (and thus reloading of the page)
@@ -178,13 +195,13 @@ class ProjectFormInput {
             let title = formInputValues[0];
             let description = formInputValues[1];
             let people = formInputValues[2];
-
+            
             let createdProject = new Project(title, description, people);
-    
+            
             // validateInput
             if (!validateObject({ value: title, required: true, minLength: 2, maxLength: 10 }) ||
-                !validateObject({ value: description, required: true, maxLength: 128 }) ||
-                !validateObject({ value: people, min: 3, max: 10 })){
+            !validateObject({ value: description, required: true, maxLength: 128 }) ||
+            !validateObject({ value: people, min: 3, max: 10 })){
                 alert('Invalid Input!');
                 return;
             } else {
@@ -194,15 +211,15 @@ class ProjectFormInput {
             }
         }
     }
-
+    
     private getFormInput(): [string, string, number] | void {
         let title = this.titleInputEl.value;
         let description = this.descrTextAreaInputEl.value;
         let peopleStr = this.peopleInputEl.value;
-
+        
         return [title, description, +peopleStr];
     }
-
+    
     private clearInput(){
         this.titleInputEl.value = '';
         this.descrTextAreaInputEl.value = '';
@@ -212,7 +229,7 @@ class ProjectFormInput {
 
 class Project {
     private _title: string = '';
-
+    
     get Title() {
         return this._title;
     }
@@ -222,40 +239,40 @@ class Project {
             console.warn('Title must be > 0 characters long');
             return;
         }
-
+        
         this._title = value;
     }
-
+    
     private _description: string;
-
+    
     get Description() {
         return this._description;
     }
-
+    
     set Description(value) {
         if(value.length <= 0){
             console.warn('Descr must be > 0 characters long');
             return;
         }
-
+        
         this._description = value;
     }
-
+    
     private _people: number;
-
+    
     get People() {
         return this._people;
     }
-
+    
     set People(value) {
         if(value >= 0){
             console.warn('Only positive number of People allowed');
             return;
         }
-
+        
         this._people = value;
     }
-
+    
     constructor(title: string, description: string, people: number) {
         this._title = title;
         this._description = description;
@@ -265,4 +282,6 @@ class Project {
 
 const renderedFormInput = new ProjectFormInput();
 
+const activeProjectCollection: ProjectCollection = new ProjectCollection('active');
+const finishedProjectCollection: ProjectCollection = new ProjectCollection('finished');
 
